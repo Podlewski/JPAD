5952a729b226b9a0a76620c31bdc7c084f8a59e2
Karol Podlewski <podlewski@hotmail.com>
2020-04-23 15:05:53 +0200

    Some stuff related to charts and correlated columns

--- Task2/main_2.py
+++ Task2/main_2.py
@@ -3,11 +3,28 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv ("mammographic_masses.data", sep=",", header=None,
-                  names=['BI-RADS','Age','Shape','Margin','Density','Severity'])
+df_orginal = pd.read_csv("mammographic_masses.data", sep=",", header=None,
+                         na_values="?", dtype=float,
+                         names=["BI-RADS","Age","Shape","Margin",
+                                 "Density","Severity"])
 
-df = df.replace('?', np.nan)
-
-percent_missing = df.isnull().sum() * 100 / len(df)
-
+percent_missing = df_orginal.isnull().sum() * 100 / len(df_orginal)
 print(percent_missing)
+
+df_wo_na = df_orginal.dropna()
+df_wo_na = df_wo_na.reset_index(drop=True).astype(int)
+
+corr = df_orginal.corr().stack()
+corr = corr[corr.index.get_level_values(0) != corr.index.get_level_values(1)]
+corr = corr.sort_values(ascending = False)
+
+col1_name = corr.index.get_level_values(0)[0]
+col2_name = corr.index.get_level_values(0)[1]
+
+x = df_wo_na[col1_name]
+y = df_wo_na[col2_name]
+
+plt.scatter(x, y)  
+plt.xlabel(col1_name)  
+plt.ylabel(col2_name)  
+plt.show()
